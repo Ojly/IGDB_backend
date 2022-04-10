30232fd76e1265069341c5abd5ff06135f99a164
ojly <ddreporter1124@yahoo.com>
2022-04-10 18:12:10 +0200

    Initialize spring project.

---
+++ src/main/java/com/igundb/IgundbApplication.java
@@ -0,0 +1,13 @@
+package com.igundb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IgundbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IgundbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/igundb/IgundbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.igundb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IgundbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
